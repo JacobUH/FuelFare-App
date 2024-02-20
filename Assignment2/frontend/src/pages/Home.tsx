513ebc57f7792d90407191d9b15863e21e0a1c72
eckybae <beckymsantos7@gmail.com>
2024-02-19 23:17:33 -0600

    finished home and contributions page

--- Assignment2/frontend/src/pages/Home.tsx
+++ Assignment2/frontend/src/pages/Home.tsx
@@ -7,12 +7,12 @@ export default function Home() {
       className={`Home ${location.pathname === "/" ? "home-background" : ""}`}
     >
       <Navbar />
-      <h1 className="my-4" style={{ fontStyle: "italic" }}>
+      <h1 className="my-4 text-center" style={{ fontStyle: "italic", fontWeight: "bold" }}>
         Find the best fuel prices for you.
       </h1>
-      <img className="img-fluid" src="./images/Tint.png"></img>
+      <img className="img-fluid" src="./images/Tint.png" style={{borderRadius: 25}}></img>
       <div className="d-grid gap-2 col-2 mx-auto">
-        <Link to="/login" className="btn btn-success my-4">
+        <Link to="/login" className="btn btn-success my-4 home-button">
           Start Here
         </Link>
       </div>
