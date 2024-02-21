82cc5f8b03787036db855b05cf739031ae24c362
SeanEppling <epplingsean@gmail.com>
2024-02-20 21:14:03 -0600

    created and styled view page

--- Assignment2/frontend/src/pages/View.tsx
+++ Assignment2/frontend/src/pages/View.tsx
@@ -1,10 +1,54 @@
 import { Navbar } from "../components/Navbar";
+import { Footer } from "../components/Footer";
+import { Link } from "react-router-dom";
 
 export default function View() {
   return (
-    <div className="View">
+    <div className={`View ${
+      location.pathname === "/view" ? "view-background" : ""
+    }`}>
       <Navbar />
-      <h1>View</h1>
+      {/*<h1>View</h1>*/}
+      <div className="container-xl">
+        <div className="card" style={{ maxWidth: "2000px", borderRadius: "15px"  }}>
+          <div className="card-body" style = {{ backgroundColor: "#88D498", borderRadius: "10px" }}>
+            <p className="card-title fs-2">Quote ID: </p>
+
+            <div className="row pb-1 mx-1">
+              <div className="col-xs text-black fs-5 p-1">
+                Estimated Quote:
+              </div>
+            </div>
+            <div className="row pb-1 mx-1">
+              <div className="col-xs text-black fs-5 p-1">
+                Date: 
+              </div>
+            </div>
+            <div className="row pb-1 mx-1">
+              <div className="col-xs text-black fs-5 p-1">
+                Gallons Requested: 
+              </div>
+            </div>
+            <div className="row pb-1 mx-1">
+              <div className="col-xs text-black fs-5 p-1">
+                Fuel Type: 
+              </div>
+            </div>
+            <div className="row pb-1 mx-1">
+              <div className="col-xs text-black fs-5 p-1">
+                Address: 
+              </div>
+            </div>
+      
+          </div>
+        </div>
+        <div style = {{ position: "absolute", right: "10px", bottom: "10px" }}>
+          <Link to="/history" className="btn btn-success bottom-0 end-0">
+                    Back
+          </Link>
+        </div>
+      </div>
+      <Footer />
     </div>
   );
 }
