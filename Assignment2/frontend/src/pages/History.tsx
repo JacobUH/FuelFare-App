2c434d7b7d7e1faa0873c84c10af37ed3a5d4b31
Anthony Delgado <115326818+AnthonyD4101@users.noreply.github.com>
2024-02-20 18:37:16 -0600

    HISTORY page nearly complete, added styling to .css

--- Assignment2/frontend/src/pages/History.tsx
+++ Assignment2/frontend/src/pages/History.tsx
@@ -1,13 +1,65 @@
 import { Navbar } from "../components/Navbar";
+import { Link } from "react-router-dom";
 
 export default function History() {
+
   return (
     <div className={`History ${
       location.pathname === "/history" ? "history-background" : ""
     }`}
     >
       <Navbar />
-      <h1 className="my-4">History</h1>
+      <h1 className="mt-3 text-center">History Page</h1>
+      <div className="mt-4">
+        <div className="row justify-content-center">
+          <div className="hist-card">
+            <div className="table-area">
+              <table className="table">
+                <thead>
+                  <tr>
+                    <th>Quote ID</th>
+                    <th style={{ textAlign: "right" }}>Date</th>
+                    <th style={{ textAlign: "right" }}>Estimated Quote</th>
+                  </tr>
+                </thead>
+                
+                <tbody>
+                  <tr>
+                    <td></td>
+                  </tr>
+                  <tr>
+                    <td></td>
+                  </tr>
+                  <tr>
+                    <td></td>
+                  </tr>
+                  <tr>
+                    <td></td>
+                  </tr>
+                  <tr>
+                    <td></td>
+                  </tr>
+                  <tr>
+                    <td></td>
+                  </tr>
+                  <tr>
+                    <td></td>
+                  </tr>
+                  <tr>
+                    <td></td>
+                  </tr>
+                  <tr>
+                    <td></td>
+                  </tr>
+                  <tr>
+                    <td></td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
