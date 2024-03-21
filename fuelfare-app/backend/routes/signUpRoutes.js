2bd1988405e4c5117ddeb57a28c2949d5e787d20
Anthony Delgado <115326818+AnthonyD4101@users.noreply.github.com>
2024-03-20 20:24:51 -0500

    cleaning to merge to dev (folder structure)

--- fuelfare-app/backend/routes/signUpRoutes.js
+++ fuelfare-app/backend/routes/signUpRoutes.js
@@ -2,7 +2,3 @@ const express = require('express');
 const router = express.Router();
 
 const signUpController = require('../controllers/signUpController');
-
-router.post('/setup', signUpController.signUpUser);
-
-module.exports = router;
