a1bd88094efda916bdd4bacb50912e60d3cabfa5
Jacob Rangel <jacobrangel0628@gmail.com>
2024-03-29 01:05:41 -0500

    finished /new quote route, api, model and controller

--- fuelfare-app/backend/models/newQuote.js
+++ fuelfare-app/backend/models/newQuote.js
@@ -4,11 +4,11 @@ const fuelTypeOptions = ["regular", "mid", "premium", "diesel"];
 const fixedPricePerGallon = 1.50; 
 
 const QuoteSchema = new mongoose.Schema({
-  numOfGallons: { type: Number, required: true, max: 50 },
+  numGallons: { type: Number, required: true, max: 1000000000 },
   fuelType: { type: String, required: true, enum: fuelTypeOptions },
   pricePerGallon: { type: Number, required: true, default: fixedPricePerGallon },
-  companyAddress1: { type: String, required: true, max: 100 },
-  deliveryDate: { type: Date, required: true },
+  address: { type: String, required: true, max: 100 },
+  deliveryDate: { type: String, required: true },
 });
 
 const NewQuote = mongoose.model("NewQuote", QuoteSchema);
