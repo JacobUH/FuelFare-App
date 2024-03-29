663be8551fe56c1c51fa5f0e911c04126b62176f
Anthony Delgado <115326818+AnthonyD4101@users.noreply.github.com>
2024-03-29 01:15:14 -0500

    add hashing to password for account creation

--- fuelfare-app/backend/controllers/setupController.js
+++ fuelfare-app/backend/controllers/setupController.js
@@ -1,10 +1,19 @@
 const Setup = require("../models/Setup");
+const bcrypt = require("bcrypt");
 
 const setup = async (req, res) => {
   try {
-    // Create a new user
-    const newUser = new Setup(req.body);
+    const { email, password, ...otherFormData } = req.body;
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = new Setup({
+      email,
+      password: hashedPassword,
+      ...otherFormData,
+    });
     await newUser.save();
+
     res.status(201).json({ message: "User signed up successfully" });
   } catch (error) {
     console.error("Error signing up:", error);
--- fuelfare-app/backend/models/Setup.js
+++ fuelfare-app/backend/models/Setup.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const UserSetupSchema = new mongoose.Schema({
   email: { type: String, required: true, maxLength: 50 },
-  password: { type: String, required: true, maxLength: 30 },
+  password: { type: String, required: true, maxLength: 200 },
   fullName: { type: String, required: true, maxLength: 100 },
   companyName: { type: String, required: true, maxLength: 50 },
   companyAddress1: { type: String, required: true, maxLength: 100 },
